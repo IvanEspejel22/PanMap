b5e6344bd90f9d4cab53669f297e8da378152f87
IvanEspejel <47361732+IvanEspejel@users.noreply.github.com>
2019-07-03 10:18:37 -0500

    Implementaci[on del menu
    Implementación del menu y se agregan paqueterías a usar

--- pan-map/src/shared/Menu.js
+++ pan-map/src/shared/Menu.js
@@ -1,22 +1,23 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-
-import styles from './Menu.css'
+import LogoBedu from '../Images/Logo-Bedu.png';
+import '../App.css';
 
 const Menu = () => {
   return (
-    <>
-      <nav>
+    <section id="NavB" class="">
+    <NavLink exact to="/"> <img class="logo-bedu" src={LogoBedu} alt="" width="120px"></img></NavLink>
+    <nav class="NavBar center-align">
         <ul>
-          <li>
-            <NavLink exact to="/" activeClassName="Menu_link">Home</NavLink>
-          </li>
-          <li>
-            <NavLink exact to="/example" activeClassName="Menu_link">Example</NavLink>
-          </li>
+           <li>
+                <NavLink exact to="/example" class="" href="login.html">
+                    <img class="icono-menu" src="usuario.svg" alt=""></img>
+                    <h5>Perfil</h5>
+                </NavLink>
+            </li>
         </ul>
-      </nav>
-    </>
+    </nav>
+</section>
   )
 }
 
